1cda1d0a55137db08c67bf0a8c5f29f707c527da
KR4MER <44881430+KR4MER@users.noreply.github.com>
2018-11-12 20:20:02 -0200

    Add files via upload

---
+++ testandoGitHub/src/testandogithub/TestandoGitHub.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testandogithub;
+
+/**
+ *
+ * @author 5220113
+ */
+public class TestandoGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**1*/System.out.println("Testando 1");
+        /**2*/System.out.println("Testando 2");
+        /**3*/System.out.println("Testando 3");
+        /**4*/System.out.println("Testando 4");
+        /**5*/System.out.println("Testando 5");
+        /**6*/
+        /**7*/
+        /**8*/
+        /**9*/
+        /**10*/
+        /**11*/
+        /**12*/
+        /**13*/
+    }
+    
+}
